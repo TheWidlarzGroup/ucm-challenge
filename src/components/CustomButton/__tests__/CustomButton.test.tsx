066a4c41d6bea70becfceea3024d5624a46e9943
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-06-21 15:43:15 +0200

    chore: run code formatter

--- src/components/CustomButton/__tests__/CustomButton.test.tsx
+++ src/components/CustomButton/__tests__/CustomButton.test.tsx
@@ -1,10 +1,10 @@
-import { render } from "@testing-library/react-native";
-import { CustomButton } from "../CustomButton";
+import { render } from '@testing-library/react-native'
+import { CustomButton } from '../CustomButton'
 
-describe("<CustomButton />", () => {
-  it("renders correctly", () => {
-    const renderAPI = render(<CustomButton />);
+describe('<CustomButton />', () => {
+  it('renders correctly', () => {
+    const renderAPI = render(<CustomButton />)
 
-    renderAPI.getByText("Button");
-  });
-});
+    renderAPI.getByText('Button')
+  })
+})
