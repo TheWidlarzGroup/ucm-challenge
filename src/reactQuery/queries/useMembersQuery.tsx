b1c8ff44804ff89b670955fc254bc4918455f943
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-06-22 12:33:42 +0200

    feat: add useMembersQuery hook

---
+++ src/reactQuery/queries/useMembersQuery.tsx
@@ -0,0 +1,42 @@
+import { getDoc, getDocs } from 'firebase/firestore'
+import { useQuery } from 'react-query'
+import { membersCollection } from '../../firebase/collections'
+import { QUERY_KEYS } from './queryKeys'
+
+const getMembersList = async () => {
+  try {
+    const members: UCM.Member[] = []
+
+    const membersSnapshot = await getDocs(membersCollection)
+
+    for (const ms of membersSnapshot.docs) {
+      const member = ms.data()
+      const jobSnapshot = await getDoc(member.job)
+
+      const memberWithJob: UCM.Member = {
+        ...member,
+        id: ms.id,
+        job: undefined,
+      }
+
+      if (jobSnapshot.exists()) {
+        const jobId = jobSnapshot.id
+        const job = { ...jobSnapshot.data(), id: jobId }
+
+        memberWithJob.job = job
+      }
+
+      members.push(memberWithJob)
+    }
+
+    return members
+  } catch (_e) {
+    return []
+  }
+}
+
+export const useMembersQuery = () => {
+  return useQuery<UCM.Member[]>([QUERY_KEYS.MEMBERS_LIST], {
+    queryFn: getMembersList,
+  })
+}
