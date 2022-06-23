0cfeacd5a17bc3da59e9ef7df89dd04548c0aef3
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-06-23 08:19:18 +0200

    test: add test utils

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,13 +2,16 @@ import { JSXElementConstructor, PropsWithChildren, ReactElement } from 'react'
 import { render, RenderOptions } from '@testing-library/react-native'
 import { ThemeProvider } from 'styled-components/native'
 import { QueryClientProvider } from 'react-query'
+import { NavigationContainer } from '@react-navigation/native'
 import { theme } from '../theme/theme'
 import { queryClient } from '../reactQuery/queryClient'
 
 const AllProviders = ({ children }: PropsWithChildren<unknown>) => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-  </QueryClientProvider>
+  <NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  </NavigationContainer>
 )
 
 const customRender = (
